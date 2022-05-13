cdb5cdd9b73657cc5722b33875375df010339c78
Hanna1224 <101805898+Hanna1224@users.noreply.github.com>
2022-05-13 12:48:27 +0800

    Delete theme.js

--- theme.js
+++
@@ -1,9 +0,0 @@
-import { orange, red } from "@material-ui/core/colors"
-import { createMuiTheme } from "@material-ui/core"
-
-export const theme = createMuiTheme({
-  palette: {
-    primary: orange,
-    secondary: red,
-  },
-})
